5793febdb0a58719f854844e6fbc47b7655f58a4
bashamahmoud <basha.mahmoud.2000@gmail.com>
2026-03-15 20:49:40 +0200

    add errors and logs middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import { healthHandler } from "./api/health.js";
+import { errorHandler, middlewareLogResponses } from "./api/middleware.js";
 
 const app = express();
 const port = process.env.PORT ?? 8080;
-
-app.get("/api/health", healthHandler);
-
+app.use(middlewareLogResponses);
+app.use("/app", express.static("./src/app"));
+app.get("/api/health", async (req, res, next) => {
+  try {
+    await healthHandler(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
+app.use(errorHandler);
 app.listen(Number(port), "0.0.0.0", () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
