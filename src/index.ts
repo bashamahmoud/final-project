ae8e02dbe02a5df9c82d44e0c75cbbd1c458e2cc
bashamahmoud <basha.mahmoud.2000@gmail.com>
2026-03-20 19:49:26 +0200

    fix the connection to DB

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import { subscribersRouter } from "./api/subscribers.js";
 
 const app = express();
 const port = process.env.PORT ?? 8080;
+app.use(express.json());
 app.use(middlewareLogResponses);
 app.use("/app", express.static("./src/app"));
 app.get("/api/health", async (req, res, next) => {
